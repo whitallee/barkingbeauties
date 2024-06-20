661cbb44d663ec2ae87afd0dbce899dfb01d718f
Whit Allee <whitallee@gmail.com>
2024-06-20 12:48:53 -0500

    added temporary event calendar and operating hours

--- components/OperatingHours.tsx
+++ components/OperatingHours.tsx
@@ -2,8 +2,8 @@ export default function OperatingHours(){
     return(
         <>
             <div id="hours" className="w-full flex flex-col items-center">
-                <h2 className="text-4xl pb-8 text-center">Operating Hours</h2>
-                
+                <h2 className="text-4xl pb-8 px-4 text-center">Operating Hours</h2>
+                <p className="px-4">Our operating hours will be determined closer to the soft-opening of our store. Feel free to reach out any time to our email if you have any questions!</p>
             </div>
         </>
     );
