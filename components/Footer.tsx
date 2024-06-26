3cab3ec1a42042ec56d26ffd96772f7839360cf4
Whit Allee <whitallee@gmail.com>
2024-06-25 20:09:56 -0500

    removed L-and-K picture and instagrams

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,12 +9,12 @@ export default function Footer(){
                 <span>Call us at <a href="tel:+17372917418" className="text-[var(--light-pink)] underline hover:text-[var(--light-blue)] hover:scale-105 transition-all">+1 (737) 291-7418</a></span>
                 <span>Or book online through <br className="min-[370px]:hidden"/>our <a href="" className="text-[var(--light-pink)] underline hover:text-[var(--light-blue)] hover:scale-105 transition-all">Booking Portal</a></span>
             </div>
-            <div className="flex flex-col gap-3">
+            {/* <div className="flex flex-col gap-3">
                 <h3>Check us out on Social Media</h3>
                 <div className="w-full h-1 rounded-full bg-[var(--neutral-green)]"></div>
                 <a href="https://www.instagram.com/groomsbylisa_atx/" className="text-[var(--light-pink)] underline flex gap-2 justify-start text-left hover:text-[var(--light-blue)] hover:scale-105 transition-all"><Instagram className="w-6 h-6 min-w-6"/>@groomsbylisa_atx</a>
                 <a href="https://www.instagram.com/_haleyalexis/" className="text-[var(--light-pink)] underline flex gap-2 justify-start text-left hover:text-[var(--light-blue)] hover:scale-105 transition-all"><Instagram className="w-6 h-6 min-w-6"/>@_haleyalexis</a>
-            </div>
+            </div> */}
             <div className="flex flex-col gap-3">
                 <h3>Common Links</h3>
                 <div className="w-full h-1 rounded-full bg-[var(--neutral-green)]"></div>
