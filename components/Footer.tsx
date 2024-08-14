9ea4f36709837704601fb5c84898fe5fe5d3c371
Whit Allee <whitallee@gmail.com>
2024-08-14 18:07:25 -0500

    finished temp landing page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,29 +1,37 @@
-import { Instagram } from "lucide-react";
+import Link from "next/link";
 
 export default function Footer(){
-    return(
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-16 bg-black text-white px-4 sm:px-16 py-8 mt-8 w-svw">
-            <div className="flex flex-col gap-3">
-                <h3>Book an Appointment</h3>
-                <div className="w-full h-1 rounded-full bg-[var(--neutral-green)]"></div>
-                <span>Call us at <a href="tel:+17372917418" className="text-[var(--light-pink)] underline hover:text-[var(--light-blue)] hover:scale-105 transition-all">+1 (737) 291-7418</a></span>
-                <span>Or book online through <br className="min-[370px]:hidden"/>our <a href="" className="text-[var(--light-pink)] underline hover:text-[var(--light-blue)] hover:scale-105 transition-all">Booking Portal</a></span>
-                {/* <span>You can also email us at <a href="mailto:" className="text-[var(--light-pink)] underline hover:text-[var(--light-blue)] hover:scale-105 transition-all">our email</a></span> */}
-            </div>
-            {/* <div className="flex flex-col gap-3">
-                <h3>Check us out on Social Media</h3>
-                <div className="w-full h-1 rounded-full bg-[var(--neutral-green)]"></div>
-                <a href="https://www.instagram.com/groomsbylisa_atx/" className="text-[var(--light-pink)] underline flex gap-2 justify-start text-left hover:text-[var(--light-blue)] hover:scale-105 transition-all"><Instagram className="w-6 h-6 min-w-6"/>@groomsbylisa_atx</a>
-                <a href="https://www.instagram.com/_haleyalexis/" className="text-[var(--light-pink)] underline flex gap-2 justify-start text-left hover:text-[var(--light-blue)] hover:scale-105 transition-all"><Instagram className="w-6 h-6 min-w-6"/>@_haleyalexis</a>
-            </div> */}
-            <div className="flex flex-col gap-3">
+    return (
+        <div>
+            <footer className="grid grid-cols-1 md:grid-cols-3 gap-16 px-8 sm:px-20 py-4">
+              <div>
+                <h3>Address</h3>
+                <div className="bg-purple-900 w-full h-[2px]"></div>
+                <div className="flex flex-col text-lg">
+                  <span>9415 Burnet Road</span>
+                  <span>Suite 100</span>
+                  <span>Austin, TX 78758</span>
+                </div>
+              </div>
+              <div>
+                <h3>Contact</h3>
+                <div className="bg-purple-900 w-full h-[2px]"></div>
+                <div className="flex flex-col text-lg">
+                  <span>737-239-5747</span>
+                  <span>barkingbeautiesps@gmail.com</span>
+                </div>
+              </div>
+              <div>
                 <h3>Common Links</h3>
-                <div className="w-full h-1 rounded-full bg-[var(--neutral-green)]"></div>
-                <a href="/" className="text-[var(--light-pink)] underline hover:text-[var(--light-blue)] hover:scale-105 transition-all">Welcome Page</a>
-                <a href="" className="text-[var(--light-pink)] underline hover:text-[var(--light-blue)] hover:scale-105 transition-all">Booking Portal</a>
-                <a href="/staff" className="text-[var(--light-pink)] underline hover:text-[var(--light-blue)] hover:scale-105 transition-all">Meet the Staff</a>
-                <a href="/faq" className="text-[var(--light-pink)] underline hover:text-[var(--light-blue)] hover:scale-105 transition-all">FAQ</a>
-            </div>
+                <div className="bg-purple-900 w-full h-[2px]"></div>
+                <div className="flex flex-col text-lg">
+                  <Link className="hover:scale-110 transition-all" href={"https://barkingbeautiesps.portal.gingrapp.com/#/public/login"}>Online Booking Portal</Link>
+                  <Link className="hover:scale-110 transition-all" href={"https://www.bbpetsalon.com/"}>Home Page</Link>
+                  <Link className="hover:scale-110 transition-all" href={"https://www.google.com/maps/dir//30.3778115,-97.7248951/@30.3769258,-97.7294504,17.22z/data=!4m2!4m1!3e0?entry=ttu"}>Google Maps Directions</Link>
+                  <Link className="hover:scale-110 transition-all" href={"https://maps.apple.com/?address=9415%20Burnet%20Rd,%20Ste%20100,%20Austin,%20TX%20%2078758,%20United%20States&ll=30.377938,-97.724759&q=9415%20Burnet%20Rd,%20Ste%20100&t=m"}>Apple Maps Directions</Link>
+                </div>
+              </div>
+            </footer>
         </div>
     )
 }
