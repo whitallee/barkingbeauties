100f320212047740ce02b523a41484de810b90c4
Whit Allee <whitallee@gmail.com>
2024-08-26 15:02:44 -0500

    flex-row on larger screens for service summary

--- components/ServicesSummary.tsx
+++ components/ServicesSummary.tsx
@@ -20,8 +20,8 @@ const fadeInAnimationVariants = {
 
 export default function ServicesSumary() {
     return (
-        <section className="w-screen max-w-full bg-[hsl(246,61%,94%)] flex flex-col gap-6 justify-center items-center text-xl py-6 px-6 lg:px-20">
-            <p className="text-center max-w-xl">Our salon specializes in skin and coat health, doodles, and cats, but we can do so much more!</p>
+        <section className="w-screen max-w-full bg-[hsl(246,61%,94%)] flex flex-col sm:flex-row gap-6 justify-center sm:justify-around items-center text-xl py-6 px-6 lg:px-20">
+            <p className="text-center sm:max-w-xs">Our salon specializes in skin and coat health, doodles, and cats, but we can do so much more!</p>
             <ul className="list-disc pl-6 flex flex-col gap-3">
                 <motion.li whileInView="animate" viewport={{once:true,}} custom={1} variants={fadeInAnimationVariants} initial="initial">Full Service Bath and Nails</motion.li>
                 <motion.li whileInView="animate" viewport={{once:true,}} custom={1} variants={fadeInAnimationVariants} initial="initial">Teeth Brushing</motion.li>
