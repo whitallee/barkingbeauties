2495d0b46b3c5ea3738b5eafefa1581dc56ff694
Whit Allee <whitallee@gmail.com>
2024-08-30 12:33:47 -0500

    removed ginger links

--- components/BookNow.tsx
+++ components/BookNow.tsx
@@ -21,7 +21,12 @@ const fadeInAnimationVariants = {
 export default function BookNow() {
     return (
         <motion.section whileInView="animate" viewport={{once:true,}} custom={0} variants={fadeInAnimationVariants} initial="initial" animate="animate" className="flex justify-center items-center py-8">
-            <Link className="text-4xl bg-white p-4 rounded-lg button-glow hover:scale-125" href="https://barkingbeautiesps.portal.gingrapp.com/#/public/login" target="_blank">Book Now</Link>
+            {/* <Link className="text-4xl bg-white p-4 rounded-lg button-glow hover:scale-125" href="https://barkingbeautiesps.portal.gingrapp.com/#/public/login" target="_blank">Book Now</Link> */}
+            <div className="text-4xl bg-white p-4 rounded-lg button-glow hover:scale-105 flex flex-col items-center justify-center text-center overflow-hidden">
+              <h3>Book Now</h3>
+              <Link className="text-sm sm:text-2xl" href={"tel:7372395747"}>By Phone: 737-239-5747</Link>
+              <Link className="text-sm sm:text-2xl" href={"mailto:barkingbeautiesps@gmail.com"}>By Email: barkingbeautiesps@gmail.com</Link>
+            </div>
         </motion.section>
     )
 }
