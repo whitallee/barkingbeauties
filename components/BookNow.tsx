a281033c2e324f8721e1de974f5da9439ff476a4
Whit Allee <whitallee@gmail.com>
2024-08-30 16:40:51 -0500

    book now button visual changes

--- components/BookNow.tsx
+++ components/BookNow.tsx
@@ -22,10 +22,10 @@ export default function BookNow() {
     return (
         <motion.section whileInView="animate" viewport={{once:true,}} custom={0} variants={fadeInAnimationVariants} initial="initial" animate="animate" className="flex justify-center items-center py-8">
             {/* <Link className="text-4xl bg-white p-4 rounded-lg button-glow hover:scale-125" href="https://barkingbeautiesps.portal.gingrapp.com/#/public/login" target="_blank">Book Now</Link> */}
-            <div className="text-4xl bg-white p-4 rounded-lg button-glow hover:scale-105 flex flex-col items-center justify-center text-center overflow-hidden">
+            <div className="text-4xl bg-white p-4 rounded-lg button-glow hover:scale-105 flex flex-col gap-2 items-center justify-center text-center overflow-hidden">
               <h3>Book Now</h3>
-              <Link className="text-sm sm:text-2xl" href={"tel:7372395747"}>By Phone: 737-239-5747</Link>
-              <Link className="text-sm sm:text-2xl" href={"mailto:barkingbeautiesps@gmail.com"}>By Email: barkingbeautiesps@gmail.com</Link>
+              <Link className="p-2 rounded-lg bg-[#351D48] text-white text-sm sm:text-2xl w-full text-nowrap" href={"tel:7372395747"}>By Phone: 737-239-5747</Link>
+              <Link className="p-2 rounded-lg bg-[#351D48] text-white text-sm sm:text-2xl w-full text-nowrap" href={"mailto:barkingbeautiesps@gmail.com"}>By Email: barkingbeautiesps@gmail.com</Link>
             </div>
         </motion.section>
     )
