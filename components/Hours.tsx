280e78ef4e9e9c16b64c2065e07ed74de02860eb
Whit Allee <whitallee@gmail.com>
2024-09-22 09:43:32 -0500

    special hours 9-22

--- components/Hours.tsx
+++ components/Hours.tsx
@@ -1,7 +1,8 @@
 export default function Hours() {
     return (
         <section className="w-screen max-w-full bg-[#351D48] text-white flex flex-col gap-6 justify-center items-center text-xl text-center py-6 px-6 lg:px-20">
-            <span>Sun/Tue/Thu/Sat<br/>Open 8:30AM - 3:00PM</span>
+            <span>*Closed Sunday 9/22*</span>
+            <span>Normal Operating Hours:<br/>Sun/Tue/Thu/Sat<br/>Open 8:30AM - 3:00PM</span>
         </section>
     )
 }
