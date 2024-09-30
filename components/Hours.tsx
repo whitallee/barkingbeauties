274c566db1c904c00caf1ba00e228090ad38cef0
Whit Allee <118332770+whitallee@users.noreply.github.com>
2024-09-30 14:31:05 -0500

    Update Hours.tsx removed 9/22 special hours

--- components/Hours.tsx
+++ components/Hours.tsx
@@ -1,7 +1,7 @@
 export default function Hours() {
     return (
         <section className="w-screen max-w-full bg-[#351D48] text-white flex flex-col gap-6 justify-center items-center text-xl text-center py-6 px-6 lg:px-20">
-            <span>*Closed Sunday 9/22*</span>
+            {/* <span>*Closed Sunday 9/22*</span> */}
             <span>Normal Operating Hours:<br/>Sun/Tue/Thu/Sat<br/>Open 8:30AM - 3:00PM</span>
         </section>
     )
