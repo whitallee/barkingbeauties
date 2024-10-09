99b6ba828ff368c60314b9752677a0972687d764
Whit Allee <whitallee@gmail.com>
2024-10-09 09:57:21 -0500

    fixed instagram links, tried tiktok but doesnt work yet

--- components/Instagrams.tsx
+++ components/Instagrams.tsx
@@ -1,11 +1,16 @@
-import { InstagramEmbed } from "react-social-media-embed";
+import { InstagramEmbed, TikTokEmbed } from "react-social-media-embed";
 
 export default function Instagrams () {
     return(
         <section className="w-screen max-w-full pt-4 px-[2px] flex flex-wrap gap-2 justify-center">
-                <InstagramEmbed url="https://www.instagram.com/p/C_1JlLzvkSK/" captioned width={326} />
-                <InstagramEmbed url="https://www.instagram.com/p/C_1Dt5GPUBa/" captioned width={326} />
-                <InstagramEmbed url="https://www.instagram.com/p/C_wGyFhPjzG/" captioned width={326} />
+                <div className="w-full flex flex-wrap gap-2 justify-center">
+                    <InstagramEmbed url="https://www.instagram.com/p/DA33VvhPye4/" captioned width={326} />
+                    <InstagramEmbed url="https://www.instagram.com/p/DA4KbXbS4eN/" captioned width={326} />
+                    <InstagramEmbed url="https://www.instagram.com/p/DA39kTAyzmR/" captioned width={326} />
+                </div>
+                {/* <div className="w-full flex flex-wrap gap-2 justify-center">
+                    <TikTokEmbed url="https://www.tiktok.com/@barkingbeautiesatx/photo/7407888826032459038?is_from_webapp=1&sender_device=pc&web_id=7423788057655412254" width={326} />
+                </div> */}
         </section>
     )
 }
