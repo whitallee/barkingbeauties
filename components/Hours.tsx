8aa2a2d91b5599c7ba9a991ab1b1e562ad841546
Whit Allee <whitallee@gmail.com>
2024-10-10 08:16:27 -0500

    changed hours to list every day per lisas request

--- components/Hours.tsx
+++ components/Hours.tsx
@@ -1,8 +1,34 @@
 export default function Hours() {
     return (
-        <section className="w-screen max-w-full bg-[#351D48] text-white flex flex-col gap-6 justify-center items-center text-xl text-center py-6 px-6 lg:px-20">
+        <section className="w-screen max-w-full bg-[#351D48] text-white flex flex-col gap-6 justify-center items-center text-xl text-center py-6 lg:px-20">
             {/* <span>*Closed Sunday 9/22*</span> */}
-            <span>Normal Operating Hours:<br/><br/>Open 8:30AM - 6:00PM<br/>Friday 10:00AM - 6:00PM<br/>Closed on Sunday</span>
+            <span>
+                <div>Operating Hours:</div>
+                <br/>
+                <div className="w-full max-w-[350px] flex-col items-center justify-center text-nowrap">
+                    <div className="w-full flex justify-between gap-6">
+                        <div>Monday</div><div>8:30AM - 6:00PM</div>
+                    </div>
+                    <div className="w-full flex justify-between gap-6">
+                        <div>Tuesday</div><div>8:30AM - 6:00PM</div>
+                    </div>
+                    <div className="w-full flex justify-between gap-6">
+                        <div>Wednesday</div><div>8:30AM - 6:00PM</div>
+                    </div>
+                    <div className="w-full flex justify-between gap-6">
+                        <div>Thursday</div><div>8:30AM - 6:00PM</div>
+                    </div>
+                    <div className="w-full flex justify-between gap-6">
+                        <div>Friday</div><div>10:00AM - 6:00PM</div>
+                    </div>
+                    <div className="w-full flex justify-between gap-6">
+                        <div>Saturday</div><div>8:30AM - 6:00PM</div>
+                    </div>
+                    <div className="w-full flex justify-between gap-6">
+                        <div>Sunday</div><div>Closed</div>
+                    </div>
+                </div>
+            </span>
         </section>
     )
 }
